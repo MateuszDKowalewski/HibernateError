ee6b98d258200fceb535d260b2b9b0f13dea27cb
Mateusz Kowalewski <30475686+MateuszDKowalewski@users.noreply.github.com>
2020-01-17 21:16:51 +0100

    Create HIbernateConfig.java

---
+++ HIbernateConfig.java
@@ -0,0 +1,21 @@
+package com.gmail.aspoka1.taniaszafa.db;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public enum HibernateConfig {
+
+    INSTANCE;
+
+    public SessionFactory getSessionFactory() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        MetadataSources sources = new MetadataSources(registry);
+        Metadata metadata = sources.getMetadataBuilder().build();
+        return metadata.getSessionFactoryBuilder().build();
+    }
+
+
+}
